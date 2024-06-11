e1f05f3ca6d45bbaea876950bc22872b0ceb6b14
Antonii Spasov <spasovantonii5@gmail.com>
2024-06-11 11:54:29 -0400

    feat: experimental event listener working

--- main.py
+++ main.py
@@ -1,19 +1,33 @@
 from connection import Connection
 from time import sleep
 
+def check_command(): 
+    return False
+
+# add args if needed =>
+def command_action(conn):
+    pass
+
 def main() -> None:
     conn: Connection = Connection()
     prefix = "/"
-    
+    last_update_id = -1
+
     while (True):
         update = conn.getUpdates()
         last_message = update["result"][len(update["result"]) - 1]["message"]
+        update_id = update["result"][len(update["result"]) - 1]["update_id"]
         text = last_message['text']
         chat_id = last_message['from']['id']
+        
+        # check id_update; 
+        # last id_update => 
 
-        if (text == f"{prefix}answer"): 
-            conn.sendMessage(chat_id, "Iamhere!")
-            break
+        if (last_update_id < update_id):
+            last_update_id = update_id
+            if (text == f"{prefix}answer"): 
+                conn.sendMessage(chat_id, "Iamhere!")
+
         sleep(1)
 
 main()
